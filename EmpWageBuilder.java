c30966c7a5bab31e54bedfceaa0fd172e479db3c
saurav <kumar00914@gmail.com>
2020-10-30 14:37:20 +0530

    Added: Employee Daily Wage Computaion

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -2,10 +2,19 @@ public class EmpWageBuilder {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program");
 		int isPresent=1;
+		int salary;
 		double empCheck = Math.floor(Math.random() * 10) % 2 ;
-		if ( empCheck == isPresent )
+		if ( empCheck == isPresent ){
 			System.out.println("Employee is Present");
-		else
+			int empRatePerHr=20;
+		   int empHrs=8;
+		   salary=empHrs*empRatePerHr;
+			System.out.println("Salary is: "+salary);
+		}
+		else{
 			System.out.println("Employee is Absent");
+			salary=0;
+			System.out.println("Salary is: "+salary);
+		}
 	}
 }
