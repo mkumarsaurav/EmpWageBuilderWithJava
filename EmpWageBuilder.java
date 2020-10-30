d221e2ee3a63601be1a024ff21160d49eeaf4b9d
saurav <kumar00914@gmail.com>
2020-10-30 13:51:36 +0530

    Added: Employee attendance checker

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -1,5 +1,11 @@
 public class EmpWageBuilder {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program");
+		int isPresent=1;
+		double empCheck = Math.floor(Math.random() * 10) % 2 ;
+		if ( empCheck == isPresent )
+			System.out.println("Employee is Present");
+		else
+			System.out.println("Employee is Absent");
 	}
 }
