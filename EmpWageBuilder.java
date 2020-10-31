8c78b2fc8c7a11a2cb1744a0110d4a012ac72588
saurav <kumar00914@gmail.com>
2020-10-31 13:02:26 +0530

    Added: Employee salary calculated for a month

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -5,27 +5,31 @@ public class EmpWageBuilder {
 		final int isPartTime=1;
 		final int isFullTime=2;
 		int empRatePerHr=20;
-		int salary,empHrs;
+		int numWorkingDays=20;
+		int salary,empHrs,empCheck,totalSalary=0;
 		Random rand=new Random();
-		int empCheck=(int)rand.nextInt(3);
-		switch(empCheck){
-		case isFullTime: {
-		   empHrs=16;
-		   System.out.println("Employee is Present");
-			break;
+		for(int day=1; day<=numWorkingDays; day++){
+			empCheck=(int)rand.nextInt(3);
+			switch(empCheck){
+				case isFullTime: {
+				   empHrs=16;
+		   		System.out.println("Employee is Present");
+					break;
+					}
+				case isPartTime:{
+					empHrs=8;
+					System.out.println("Employee is Present Part-Time");
+					break;
+					}
+				default: {
+					System.out.println("Employee is Absent");
+					empHrs=0;
+					break;
+					}
+			}
+			salary=empHrs*empRatePerHr;
+			totalSalary=totalSalary+salary;
 		}
-		case isPartTime:{
-			empHrs=8;
-			System.out.println("Employee is Present Part-Time");
-			break;
-		}
-		default: {
-			System.out.println("Employee is Absent");
-			empHrs=0;
-			break;
-		}
-		}
-		salary=empHrs*empRatePerHr;
-		System.out.println("Salary is: "+salary);
+			System.out.println("Salary for a Month is: "+totalSalary);
 	}
 }
