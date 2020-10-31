38ee2cc838fd764762910615248ffcc287672770
saurav <kumar00914@gmail.com>
2020-10-31 12:34:04 +0530

    Refactored Using switch case for part time wage computation of employee

--- EmpWageBuilder.java
+++ EmpWageBuilder.java
@@ -2,23 +2,28 @@ import java.util.*;
 public class EmpWageBuilder {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program");
-		int isPartTime=1;
-		int isFullTime=2;
+		final int isPartTime=1;
+		final int isFullTime=2;
 		int empRatePerHr=20;
 		int salary,empHrs;
 		Random rand=new Random();
-		int empCheck=rand.nextInt(3);
-		if ( empCheck == isFullTime ){
+		int empCheck=(int)rand.nextInt(3);
+		switch(empCheck){
+		case isFullTime: {
 		   empHrs=16;
 		   System.out.println("Employee is Present");
+			break;
 		}
-		else if ( empCheck == isPartTime ){
+		case isPartTime:{
 			empHrs=8;
 			System.out.println("Employee is Present Part-Time");
+			break;
 		}
-		else {
+		default: {
 			System.out.println("Employee is Absent");
 			empHrs=0;
+			break;
+		}
 		}
 		salary=empHrs*empRatePerHr;
 		System.out.println("Salary is: "+salary);
